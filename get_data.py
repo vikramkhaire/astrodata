6f271210372dd04a5db38fd04e9ee520e3081964
Vikram <vickykhaire@gmail.com>
2020-06-13 11:52:50 -0700

    corrections

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
 import astropy.table as tab
 import numpy as np
+import os
 
 def mean_igm_flux(paper = 'Becker13'):
     """
@@ -10,8 +11,10 @@ def mean_igm_flux(paper = 'Becker13'):
     exmaple: 
     z, f, e = mean_igm_flux()
     """
-    if paper = 'Becker13':
-        data = tab.Table.read('./igm/mean_flux_Becker13.fits')
+    path_of_this_file =  os.path.dirname(__file__)
+
+    if paper == 'Becker13':
+        data = tab.Table.read(path_of_this_file +'/igm/mean_flux_Becker13.fits')
         print('Cite: Becker et al 2013 paper (https://ui.adsabs.harvard.edu/abs/2007ApJ...662...72B/abstract)')
 
     return data['z'], data['mean_flux'], data['error']
