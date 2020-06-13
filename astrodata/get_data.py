78724b0fda9ff08fffbab2a67e0f2b9df2dfbf4f
Vikram <vickykhaire@gmail.com>
2020-06-13 15:21:38 -0700

    add deleted files

---
+++ astrodata/get_data.py
@@ -0,0 +1,22 @@
+import astropy.table as tab
+import numpy as np
+import os
+
+def mean_igm_flux(paper = 'Becker13'):
+    """
+    :param paper: default is Becker13
+    :return: z, mean flux, 1 sigma error
+    """
+    """
+    exmaple: 
+    z, f, e = mean_igm_flux()
+    """
+
+    path_of_this_file =  os.path.dirname(__file__)
+
+    if paper == 'Becker13':
+        data = tab.Table.read(path_of_this_file +'/igm/mean_flux_Becker13.fits')
+        print('Cite: Becker et al 2013 paper (https://ui.adsabs.harvard.edu/abs/2007ApJ...662...72B/abstract)')
+
+
+    return data['z'], data['mean_flux'], data['error']
